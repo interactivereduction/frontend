5b3536d381b8ad36edf6fc792889b9885316caff
Dagonite <samuel.stock@stfc.ac.uk>
2023-05-11 03:56:12 +0100

    Initial react app

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+// export default App;
+
+function MyButton() {
+  return (
+    <button>
+      Test button
+    </button>
+  );
+}
+
+export default function InteractiveReduction() {
+  return (
+    <div>
+      <h1>Welcome to Interactive Reduction</h1>
+      <MyButton />
+    </div>
+  );
+}
+
