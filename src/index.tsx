b6f4869999b8baf087c1394a752bcd9d592a132d
Dagonite <samuel.stock@stfc.ac.uk>
2023-05-12 03:44:04 +0100

    Update to work as SG plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,125 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import singleSpaReact from 'single-spa-react';
+import TestBedComponent from './testbed/testbed.component';
+import * as log from 'loglevel';
+import { createRoute } from './routes';
+import { createWebsocketClient } from './websocket';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+if (process.env.NODE_ENV === `development`) {
+  ReactDOM.render(
+    <TestBedComponent pluginName="Demo Plugin">
+      <App />
+    </TestBedComponent>,
+    document.getElementById('frontend')
+  );
+  log.setDefaultLevel(log.levels.DEBUG);
+} else {
+  log.setDefaultLevel(log.levels.ERROR);
+}
+
+function domElementGetter(): HTMLElement {
+  // Make sure there is a div for us to render into
+  let el = document.getElementById('frontend');
+  if (!el) {
+    el = document.createElement('div');
+  }
+  return el;
+}
+
+// Create WebSocket client to respond to listen for pushed notifications
+createWebsocketClient('ws://localhost:3210/');
+
+const reactLifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  renderType: 'render',
+  rootComponent: App,
+  domElementGetter,
+  errorBoundary: (error): React.ReactElement => {
+    log.error(`frontend failed with error: ${error}`);
+
+    return (
+      <div className="error">
+        <div
+          style={{
+            padding: 20,
+            background: 'red',
+            color: 'white',
+            margin: 5,
+          }}
+        >
+          Something went wrong...
+        </div>
+      </div>
+    );
+  },
+});
+
+// these are examples of route registration events being fired back to the parent
+createRoute('Data', 'Demo Plugin', '/plugin1', 10, 'Data help text');
+createRoute(
+  'Analysis',
+  'Demo Plugin Analysis',
+  '/plugin1/analysis',
+  4,
+  'Analysis help text'
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const render = (): void => {
+  const el = document.getElementById('frontend');
+  if (el) {
+    ReactDOM.render(<App />, document.getElementById('frontend'));
+  }
+};
+
+window.addEventListener('single-spa:routing-event', () => {
+  // attempt to re-render the plugin if the corresponding div is present
+  render();
+});
+
+document.addEventListener('scigateway', (e) => {
+  // attempt to re-render the plugin if the corresponding div is present
+  const action = (e as CustomEvent).detail;
+  if (action.type === 'scigateway:api:plugin_rerender') {
+    console.log('rerendering...');
+    render();
+  }
+});
+
+// Single-SPA bootstrap methods have no idea what type of inputs may be
+// pushed down from the parent app
+export function bootstrap(props: unknown): Promise<void> {
+  return reactLifecycles
+    .bootstrap(props)
+    .then(() => {
+      log.info('frontend has been successfully bootstrapped');
+    })
+    .catch((error: unknown) => {
+      log.error('frontend failed whilst bootstrapping: ' + error);
+    });
+}
+
+export function mount(props: unknown): Promise<void> {
+  return reactLifecycles
+    .mount(props)
+    .then(() => {
+      log.info('frontend has been successfully mounted');
+    })
+    .catch((error: unknown) => {
+      log.error('frontend failed whilst mounting: ' + error);
+    });
+}
+
+export function unmount(props: unknown): Promise<void> {
+  return reactLifecycles
+    .unmount(props)
+    .then(() => {
+      log.info('frontend has been successfully unmounted');
+    })
+    .catch((error: unknown) => {
+      log.error('frontend failed whilst unmounting: ' + error);
+    });
+}
---
+++ src/routes.tsx
@@ -0,0 +1,22 @@
+export function createRoute(
+  section: string,
+  label: string,
+  route: string,
+  order: number,
+  helpText: string
+): void {
+  const routeAction = {
+    type: 'scigateway:api:register_route',
+    payload: {
+      section: section,
+      link: route,
+      plugin: 'frontend',
+      displayName: label,
+      order: order,
+      helpText: helpText,
+    },
+  };
+  document.dispatchEvent(
+    new CustomEvent('scigateway', { detail: routeAction })
+  );
+}
