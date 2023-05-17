98705f859b6417e2fb132de58a7014ba2063434d
Dagonite <samuel.stock@stfc.ac.uk>
2023-05-17 08:35:20 +0100

    Update test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders my button', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+    const buttonElement = screen.getByText(/my button/i);
+    expect(buttonElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,9 @@ function MyButton() {
 
 export default function InteractiveReduction() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to Interactive Reduction</h1>
+      <MyButton />
     </div>
   );
 }
