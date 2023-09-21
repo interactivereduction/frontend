53eef1df0d60e7bc18f27924e8e71ca11980ad37
Dagonite <samuel.stock@stfc.ac.uk>
2023-09-21 08:50:36 +0100

    Add event listener for parent theme
    Apply theme to whole of app

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,27 @@ import Instrument from './Instrument';
 import LiveReduction from './LiveReduction';
 import ReductionHistory from './ReductionHistory';
 import HomePage from './HomePage';
+import GlobalStyles from './GlobalStyles';
 
 const App: FC = () => (
-  <Router basename="/ir">
-    <Switch>
-      <Route exact path="/">
-        <HomePage />
-      </Route>
-      <Route path="/instruments">
-        <Instrument />
-      </Route>
-      <Route path="/:instrumentName/live_reduction">
-        <LiveReduction />
-      </Route>
-      <Route path="/:instrumentName/reduction_history">
-        <ReductionHistory />
-      </Route>
-    </Switch>
-  </Router>
+  <GlobalStyles>
+    <Router basename="/ir">
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/instruments">
+          <Instrument />
+        </Route>
+        <Route path="/:instrumentName/live_reduction">
+          <LiveReduction />
+        </Route>
+        <Route path="/:instrumentName/reduction_history">
+          <ReductionHistory />
+        </Route>
+      </Switch>
+    </Router>
+  </GlobalStyles>
 );
 
 export default App;
