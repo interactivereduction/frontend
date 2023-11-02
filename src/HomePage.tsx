2c3ec579375c92bf06a42153d3ba4d611ff15ccd
Dagonite <samuel.stock@stfc.ac.uk>
2023-11-02 05:11:50 +0000

    Add new home page

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,117 +1,372 @@
-import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
-import Button from '@mui/material/Button';
-import SearchIcon from '@mui/icons-material/Search';
+import React from 'react';
 import Typography from '@mui/material/Typography';
+import {
+  Grid,
+  Box,
+  Paper,
+  Button,
+  Avatar,
+  alpha,
+  styled,
+  useMediaQuery,
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import DownloadIcon from '@mui/icons-material/GetApp';
+import { Trans, useTranslation } from 'react-i18next';
+import BackgroundImage from './images/background.jpg';
+import GreenSwirl1Image from './images/green-swirl1.png';
+import GreenSwirl2Image from './images/green-swirl2.png';
+import Decal1Image from './images/decal1.svg';
+import Decal2Image from './images/decal2.svg';
+import Decal2DarkImage from './images/decal2-dark.svg';
+import Decal2DarkHCImage from './images/decal2-darkhc.svg';
+import FacilityImage from './images/facility.jpg';
+import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
 
-import { styles } from './HomePage.styles';
+export interface HomePageProps {
+  logo: string;
+  backgroundImage: string;
+  greenSwirl1Image: string;
+  greenSwirl2Image: string;
+  decal1Image: string;
+  decal2Image: string;
+  decal2DarkImage: string;
+  decal2DarkHCImage: string;
+  facilityImage: string;
+}
 
-const HomePage: FC = () => {
+const backgroundTitleStyles = {
+  color: '#FFFFFF',
+  margin: 'auto',
+  fontSize: '48px',
+  fontWeight: 'lighter',
+  textAlign: 'center',
+};
+
+const backgroundTitleStylesMobile = {
+  ...backgroundTitleStyles,
+  fontSize: '32px',
+  marginLeft: '8px',
+  marginRight: '8px',
+};
+
+const paperStyles = {
+  borderRadius: '4px',
+  marginBottom: 2,
+  height: '100%',
+  width: '100%',
+};
+
+const avatarStyles = {
+  backgroundColor: '#1E5DF8',
+  color: '#FFFFFF',
+  width: '60px',
+  height: '60px',
+  marginBottom: 2,
+};
+
+const paperContentStyles = {
+  padding: 2,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  height: '100%',
+  boxSizing: 'border-box',
+};
+
+const avatarIconStyles = {
+  transform: 'scale(1.75)',
+};
+
+const PaperHeading = styled(Typography)(({ theme }) => ({
+  fontWeight: 'bold',
+  fontSize: '24px',
+  color: (theme as any).colours?.homePage?.heading,
+  marginBottom: theme.spacing(2),
+}));
+
+const PaperDescription = styled(Typography)(({ theme }) => ({
+  textAlign: 'left',
+  color: (theme as any).colours?.contrastGrey,
+  marginBottom: theme.spacing(2),
+}));
+
+const BluePaperHeading = styled(Typography)(({ theme }) => ({
+  fontWeight: 'bold',
+  fontSize: '24px',
+  color: '#FFFFFF',
+  marginBottom: theme.spacing(2),
+}));
+
+const BluePaperDescription = styled(Typography)(({ theme }) => ({
+  textAlign: 'left',
+  color: (theme as any).colours?.homePage?.blueDescription,
+  marginBottom: theme.spacing(2),
+}));
+
+interface BrowseDecalProps {
+  decal2Image: string;
+  decal2DarkImage: string;
+  decal2DarkHCImage: string;
+}
+
+const BrowseDecal = styled('div', {
+  shouldForwardProp: (prop) =>
+    prop !== 'decal2Image' &&
+    prop !== 'decal2DarkImage' &&
+    prop !== 'decal2DarkHCImage',
+})<BrowseDecalProps>(
+  ({ theme, decal2Image, decal2DarkImage, decal2DarkHCImage }) => ({
+    backgroundImage:
+      theme.palette.mode === 'light'
+        ? `url(${decal2Image})`
+        : (theme as any).colours?.type === 'default'
+        ? `url(${decal2DarkImage})`
+        : `url(${decal2DarkHCImage})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'top left',
+    backgroundSize: 'auto 100%',
+    height: '100%',
+  })
+);
+
+const LightBlueButton = styled(Button)(({ theme }) => ({
+  color: '#FFFFFF',
+  backgroundColor: (theme as any).colours?.homePage?.blueButton,
+  '&:hover': {
+    // Check if null to avoid error when loading
+    backgroundColor: (theme as any).colours?.homePage?.blueButton
+      ? alpha((theme as any).colours?.homePage?.blueButton, 0.8)
+      : '#FFFFFF',
+  },
+}));
+
+const HomePage = (): React.ReactElement => {
+  const [t] = useTranslation();
   const theme = useTheme();
+  const isViewportMdOrLager = useMediaQuery(theme.breakpoints.up('md'));
+
   return (
-    <div style={styles.container}>
-      <Typography
-        variant="h1"
+    <div id="ir-homepage">
+      <div
         style={{
-          ...styles.title,
-          color: theme.palette.text.primary,
-        }} // manually set color for now
+          backgroundImage: `url(${BackgroundImage})`,
+          backgroundPosition: 'center 40%',
+          width: '100%',
+          height: 250,
+        }}
       >
-        <strong>Data reduction</strong> and <strong>processing</strong> <br />
-        for <strong>large-scale</strong> science facilities
-      </Typography>
-      <div style={styles.boxes}>
-        <div style={styles.largeBox}>
-          <Typography variant="h4" component="h1" style={styles.boxHeading}>
-            Reduce and perform basic analysis remotely from a clean web
-            interface
-          </Typography>
-          <Typography variant="body1" style={styles.largeBoxText}>
-            Large scale facilities, such as synchrotrons, neutron and muon
-            sources, lasers and accelerators, generate vast amounts of data that
-            need to be managed in an efficient way, supporting data ingestion
-            for long-term storage and archival, as well as data analysis and
-            data publication workflows.
-          </Typography>
-          <Typography variant="body1" style={styles.largeBoxText}>
-            Interactive Reduction focuses on providing automatic reduction and
-            live reduction functionality interactively from the web.
-          </Typography>
-          <Link to="instruments" style={{ textDecoration: 'none' }}>
-            <Button variant="contained" style={styles.button}>
-              Browse instruments
-            </Button>
-          </Link>
-        </div>
-
-        <div style={styles.secondRow}>
-          <div style={styles.smallBox}>
-            <div style={styles.search}>
-              <SearchIcon style={{ color: 'white' }} />
-            </div>
-            <Typography variant="h5" component="h2" style={styles.boxHeading}>
-              Live reduction
-            </Typography>
-            <Typography variant="body1" style={styles.smallBoxText}>
-              Browse a list of instruments and access the live data
-              functionality for a specific instrument.
-            </Typography>
-            <Link to="instruments" style={{ textDecoration: 'none' }}>
-              <Button variant="contained" style={styles.button}>
-                Browse instruments
-              </Button>
-            </Link>
-          </div>
-          <div
-            style={{
-              ...styles.smallBox,
-              margin: '0 10px',
+        <div
+          style={{
+            backgroundImage: `url(${GreenSwirl1Image}), url(${Decal1Image})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'top left, top right',
+            width: '100%',
+            height: 250,
+          }}
+        >
+          <Box
+            sx={{
+              position: 'relative',
+              left: '50%',
+              top: '45px',
+              transform: 'translate(-50%)',
             }}
           >
-            <div style={styles.search}>
-              <SearchIcon style={{ color: 'white' }} />
-            </div>
-            <Typography variant="h5" component="h2" style={styles.boxHeading}>
-              Histrorical reductions
-            </Typography>
-            <Typography variant="body1" style={styles.smallBoxText}>
-              Browse and search a list of all reductions performed on the
-              platform.
-            </Typography>
-            <Link to="history" style={{ textDecoration: 'none' }}>
-              <Button variant="contained" style={styles.button}>
-                Browse reductions
-              </Button>
-            </Link>
-          </div>
-
-          <div
-            style={{
-              ...styles.smallBox,
-              backgroundColor: 'darkblue',
-            }}
-          >
-            <Typography variant="h5" component="h2" style={styles.boxHeading}>
-              ISIS Neutron and Muon Source
-            </Typography>
-            <Typography variant="body1" style={styles.smallBoxText}>
-              World-leading centre for research giving unique insights into the
-              properties of materials on the atomic scale.
-            </Typography>
-            <a
-              style={{ textDecoration: 'none' }}
-              href="https://www.isis.stfc.ac.uk/Pages/About.aspx"
-              target="_blank"
-              rel="noopener noreferrer"
+            <Typography
+              variant="h2"
+              sx={
+                isViewportMdOrLager
+                  ? backgroundTitleStyles
+                  : backgroundTitleStylesMobile
+              }
             >
-              <Button variant="contained" style={styles.button}>
-                Read more
-              </Button>
-            </a>
-          </div>
+              <Trans i18nKey="home-page.title_line1">
+                <strong>Data reduction</strong> and <strong>processing</strong>
+              </Trans>
+            </Typography>
+            <Typography
+              variant="h2"
+              sx={
+                isViewportMdOrLager
+                  ? backgroundTitleStyles
+                  : backgroundTitleStylesMobile
+              }
+            >
+              <Trans i18nKey="home-page.title_line2">
+                for <strong>large-scale</strong> science facilities
+              </Trans>
+            </Typography>
+          </Box>
         </div>
       </div>
+      <Box
+        sx={{
+          transform: 'translate(0px, -20px)',
+          marginLeft: isViewportMdOrLager ? '8%' : '8px',
+          marginRight: isViewportMdOrLager ? '8%' : '8px',
+        }}
+      >
+        <Paper sx={paperStyles} elevation={1}>
+          <Grid container style={{ height: '100%' }}>
+            <Grid item xs={12} md={6}>
+              <Box sx={paperContentStyles}>
+                <Typography
+                  variant="h3"
+                  sx={(theme) => ({
+                    fontWeight: 'bold',
+                    fontSize: '32px',
+                    color: (theme as any).colours?.homePage?.heading,
+                    marginBottom: theme.spacing(2),
+                  })}
+                >
+                  {t(
+                    'Reduce and perform basic analysis remotely from a clean web interface'
+                  )}
+                </Typography>
+                <PaperDescription variant="body1">
+                  {t(
+                    'Large scale facilities, such as synchrotrons, neutron and muon sources, lasers and accelerators, generate vast amounts of data that need to be managed in an efficient way, supporting data ingestion for long-term storage and archival, as well as data analysis and data publication workflows.'
+                  )}
+                </PaperDescription>
+                <PaperDescription variant="body1">
+                  <Trans i18nKey="home-page.browse.description2">
+                    <strong>Interactive Reduction</strong> focuses on providing
+                    scientists an interface to perform automatic reductions for
+                    beamline instruments from the web.
+                  </Trans>
+                </PaperDescription>
+                <Box marginTop="16px">
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    component={Link}
+                    to={t('instruments')}
+                    data-testid="browse-button"
+                  >
+                    {t('Browse instruments')}
+                  </Button>
+                </Box>
+              </Box>
+            </Grid>
+            {isViewportMdOrLager && (
+              <Grid item md={6}>
+                <div
+                  style={{
+                    backgroundImage: `url(${FacilityImage})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'bottom right',
+                    backgroundSize: 'cover',
+                    width: '100%',
+                    height: '100%',
+                    borderRadius: '4px',
+                  }}
+                >
+                  <BrowseDecal
+                    decal2Image={Decal2Image}
+                    decal2DarkImage={Decal2DarkImage}
+                    decal2DarkHCImage={Decal2DarkHCImage}
+                  />
+                </div>
+              </Grid>
+            )}
+          </Grid>
+        </Paper>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <Paper sx={paperStyles} elevation={1}>
+              <Box sx={paperContentStyles}>
+                <Avatar sx={avatarStyles}>
+                  <SearchIcon sx={avatarIconStyles} />
+                </Avatar>
+                <PaperHeading variant="h4">{t('Live reductions')}</PaperHeading>
+                <PaperDescription variant="body1">
+                  {t(
+                    'Browse the instrument page to select an instrument to access the live data functionality for a specific instrument.'
+                  )}
+                </PaperDescription>
+                <Box marginTop="auto">
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    component={Link}
+                    to={t('instruments')}
+                    data-testid="browse-button"
+                  >
+                    {t('Browse instruments')}
+                  </Button>
+                </Box>
+              </Box>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Paper sx={paperStyles} elevation={1}>
+              <Box sx={paperContentStyles}>
+                <Avatar sx={avatarStyles}>
+                  <SearchIcon sx={avatarIconStyles} />
+                </Avatar>
+                <PaperHeading variant="h4">
+                  {t('Historical reductions')}
+                </PaperHeading>
+                <PaperDescription variant="body1">
+                  {t(
+                    'Browse and search a list of all reduction performed on the platform.'
+                  )}
+                </PaperDescription>
+                <Box marginTop="auto">
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    component={Link}
+                    to={t('history')}
+                    data-testid="browse-button"
+                  >
+                    {t('Browse reductions')}
+                  </Button>
+                </Box>
+              </Box>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Paper
+              sx={{ ...paperStyles, backgroundColor: '#003088' }}
+              elevation={1}
+            >
+              <div
+                style={{
+                  backgroundImage: `url(${GreenSwirl2Image})`,
+                  backgroundRepeat: 'no-repeat',
+                  backgroundPosition: 'top right',
+                  backgroundSize: 'auto 100%',
+                  height: '100%',
+                }}
+              >
+                <Box sx={paperContentStyles}>
+                  <BluePaperHeading variant="h4">
+                    {t('ISIS Neutron and Muon Source')}
+                  </BluePaperHeading>
+                  <BluePaperDescription variant="body1">
+                    {t(
+                      'World-leading centre for research giving unique insights into the properties of materials on the atomic scale.'
+                    )}
+                  </BluePaperDescription>
+                  <Box marginTop="auto">
+                    <LightBlueButton
+                      color="primary"
+                      variant="contained"
+                      href={t('https://www.isis.stfc.ac.uk/Pages/About.aspx')}
+                      data-testid="facility-button"
+                    >
+                      {t('Read more')}
+                    </LightBlueButton>
+                  </Box>
+                </Box>
+              </div>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 };
