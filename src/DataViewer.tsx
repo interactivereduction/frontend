7f35848e5262e801922c290c4231a55b90d3f421
Dagonite <samuel.stock@stfc.ac.uk>
2024-03-28 00:35:55 +0000

    Make plot respect styling theme

--- src/DataViewer.tsx
+++ src/DataViewer.tsx
@@ -27,11 +27,14 @@ const DataViewer: React.FC = () => {
     history.push(`/data-viewer/${newInstrument}`);
   };
 
+  const backgroundColor = theme.palette.mode === 'dark' ? '#282828' : 'white';
+  const textColor = theme.palette.text.primary;
+
   return (
     <div style={{ padding: '20px' }}>
       <Box display="flex" alignItems="center" justifyContent="space-between" marginBottom="20px">
         <Typography variant="h3" component="h1" style={{ color: theme.palette.text.primary }}>
-          {selectedInstrument.toUpperCase()} data viewer
+          {selectedInstrument.toUpperCase()} Data viewer
         </Typography>
 
         <FormControl style={{ minWidth: '200px' }}>
@@ -62,7 +65,16 @@ const DataViewer: React.FC = () => {
           },
           { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
         ]}
-        layout={{ width: 720, height: 440, title: 'A Test Plot' }}
+        layout={{
+          width: 720,
+          height: 440,
+          title: 'A Test Plot',
+          paper_bgcolor: backgroundColor,
+          plot_bgcolor: backgroundColor,
+          font: {
+            color: textColor,
+          },
+        }}
       />
     </div>
   );
