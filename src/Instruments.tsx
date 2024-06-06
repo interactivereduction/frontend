f0171b7226f93621e111fe54d85b4e99c7c8fd79
Dagonite <samuel.stock@stfc.ac.uk>
2024-06-06 16:32:58 +0100

    Add favouriting logic

--- src/Instruments.tsx
+++ src/Instruments.tsx
@@ -5,6 +5,8 @@ import { Link as RouterLink } from 'react-router-dom';
 // Material UI components
 import { Box, Button, Collapse, IconButton, Link, List, ListItem, Typography, useTheme } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarIcon from '@mui/icons-material/Star';
 
 // Local data
 import { instruments } from './InstrumentData';
@@ -12,6 +14,7 @@ import { instruments } from './InstrumentData';
 const Instruments: React.FC = () => {
   const theme = useTheme();
   const [expandedIds, setExpandedIds] = React.useState<number[]>([]);
+  const [favoriteIds, setFavoriteIds] = React.useState<number[]>([]);
 
   const handleToggleExpand = (id: number, event?: React.MouseEvent): void => {
     if (event) {
@@ -24,13 +27,32 @@ const Instruments: React.FC = () => {
     );
   };
 
+  const handleToggleFavorite = (id: number, event: React.MouseEvent): void => {
+    event.stopPropagation();
+    setFavoriteIds((prevFavoriteIds) =>
+      prevFavoriteIds.includes(id)
+        ? prevFavoriteIds.filter((favoriteId) => favoriteId !== id)
+        : [...prevFavoriteIds, id]
+    );
+  };
+
+  const sortedInstruments = [...instruments].sort((a, b) => {
+    if (favoriteIds.includes(a.id) && !favoriteIds.includes(b.id)) {
+      return -1;
+    }
+    if (!favoriteIds.includes(a.id) && favoriteIds.includes(b.id)) {
+      return 1;
+    }
+    return 0;
+  });
+
   return (
     <>
       <Typography variant="h3" component="h1" style={{ color: theme.palette.text.primary, padding: '20px' }}>
         ISIS instruments
       </Typography>
       <Box sx={{ paddingBottom: '2rem' }}>
-        {instruments.map((instrument) => (
+        {sortedInstruments.map((instrument) => (
           <Box
             key={instrument.id}
             sx={{ marginBottom: 1, marginLeft: 2, marginRight: 2 }}
@@ -46,33 +68,45 @@ const Instruments: React.FC = () => {
                   : theme.palette.background.paper,
               }}
             >
-              <Box display="flex" alignItems="center">
-                <IconButton
-                  aria-expanded={expandedIds.includes(instrument.id)}
-                  aria-label="show more"
-                  onClick={(event) => handleToggleExpand(instrument.id, event)}
-                >
-                  <ExpandMoreIcon
-                    style={{ transform: expandedIds.includes(instrument.id) ? 'rotate(180deg)' : 'none' }}
-                  />
-                </IconButton>
-                <Box sx={{ marginLeft: 2 }}>
-                  <Typography
-                    variant="h6"
-                    component="h1"
-                    style={{
-                      fontSize: '1.2rem',
-                      fontWeight: 'bold',
-                      textTransform: 'uppercase',
-                      color: theme.palette.mode === 'dark' ? '#86b4ff' : theme.palette.primary.main,
-                    }}
+              <Box display="flex" alignItems="center" justifyContent="space-between">
+                <Box display="flex" alignItems="center">
+                  <IconButton
+                    aria-expanded={expandedIds.includes(instrument.id)}
+                    aria-label="show more"
+                    onClick={(event) => handleToggleExpand(instrument.id, event)}
                   >
-                    {instrument.name}
-                  </Typography>
-                  <Typography sx={{ color: theme.palette.text.primary }} variant="body1">
-                    {instrument.type}
-                  </Typography>
+                    <ExpandMoreIcon
+                      style={{ transform: expandedIds.includes(instrument.id) ? 'rotate(180deg)' : 'none' }}
+                    />
+                  </IconButton>
+                  <Box sx={{ marginLeft: 2 }}>
+                    <Typography
+                      variant="h6"
+                      component="h1"
+                      style={{
+                        fontSize: '1.2rem',
+                        fontWeight: 'bold',
+                        textTransform: 'uppercase',
+                        color: theme.palette.mode === 'dark' ? '#86b4ff' : theme.palette.primary.main,
+                      }}
+                    >
+                      {instrument.name}
+                    </Typography>
+                    <Typography sx={{ color: theme.palette.text.primary }} variant="body1">
+                      {instrument.type}
+                    </Typography>
+                  </Box>
                 </Box>
+                <IconButton
+                  aria-label="add to favorites"
+                  onClick={(event) => handleToggleFavorite(instrument.id, event)}
+                >
+                  {favoriteIds.includes(instrument.id) ? (
+                    <StarIcon style={{ color: theme.palette.warning.main }} />
+                  ) : (
+                    <StarBorderIcon />
+                  )}
+                </IconButton>
               </Box>
               <Collapse in={expandedIds.includes(instrument.id)} timeout="auto" unmountOnExit>
                 <Box marginTop={2}>
