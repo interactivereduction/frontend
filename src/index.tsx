722a7bbf11a6f11f0b44cc5f5e0127c7e456444c
Keiran Price <keiran.price@stfc.ac.uk>
2024-06-21 15:16:53 +0100

    Derestrict single page

--- src/index.tsx
+++ src/index.tsx
@@ -62,10 +62,10 @@ createRoute(
   '/fia', // route the link should link to
   1, // how high up in the section should your link be - ascending order
   'Data help text', // help text renders a tooltip in the site tour for this link
-  true // whether the link should be visible to unauthenticated users
+  false // whether the link should be visible to unauthenticated users
 );
-createRoute('Reductions', 'Instruments', '/fia/instruments', 2, 'Data help text', true);
-createRoute('Reductions', 'Reduction history', '/fia/reduction-history/ALF', 3, 'Data help text', true);
+createRoute('Reductions', 'Instruments', '/fia/instruments', 2, 'Data help text', false);
+createRoute('Reductions', 'Reduction history', '/fia/reduction-history/ALF', 3, 'Data help text', false);
 
 // Single-SPA bootstrap methods have no idea what type of inputs may be
 // pushed down from the parent app
